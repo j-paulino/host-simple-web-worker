1936126b86fff7f3c73c347cb30fabf2a1892847
Joel Paulino <joel@joelpaulino.com>
2016-05-18 00:53:09 -0700

    Throw an actual error.

--- simple_worker.js
+++ simple_worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('message', function(e) {
-    throw 'Error Yo';
+    throw Error('Error Yo');
     // Send the message back.
     self.postMessage('You said: ' + e.data);
 }, false);
