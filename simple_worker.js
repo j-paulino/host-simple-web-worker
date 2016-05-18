4badad89b748e5efd3589b8ee693fe83fe8764f7
Joel Paulino <joel@joelpaulino.com>
2016-05-18 00:55:33 -0700

    Update simple_worker.js

--- simple_worker.js
+++ simple_worker.js
@@ -1,5 +1,9 @@
 self.addEventListener('message', function(e) {
-    throw Error('Error Yo');
     // Send the message back.
+    if(e.data === "throw-error"){
+         throw Error('Error Yo');
+    }
+    
     self.postMessage('You said: ' + e.data);
+    
 }, false);
