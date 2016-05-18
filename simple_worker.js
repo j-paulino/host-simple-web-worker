da25b22b8905cc37e6c805945291b5f252da640f
Joel Paulino <joel@joelpaulino.com>
2016-05-18 08:37:15 -0700

    Be naughty.

--- simple_worker.js
+++ simple_worker.js
@@ -1,3 +1,4 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.5/angular.min.js');
 self.addEventListener('message', function(e) {
     // Send the message back.
     if(e.data === "throw-error"){
