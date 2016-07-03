9273fac634bc3ca9afa320661508042944dc87d5
Joel Paulino <joel@joelpaulino.com>
2016-07-03 13:34:17 -0700

    Update simple_worker.js

--- simple_worker.js
+++ simple_worker.js
@@ -1,17 +1,9 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.13.1/lodash.min.js')
-importScripts('js/script1.js');
-importScripts('js/script2.js','js/script3.js');
 
 self.addEventListener('message', function(e) {
     var msg  = e.data;
-    // Send the message back.
-    if(e.data === "throw-error"){
-         throw Error('Error Yo');
-    }
     
-    if(e.data === "__angular__"){
-        msg = typeof angular;
-    }
+    console.log(_);
     
     self.postMessage('You said: ' + msg);
     
